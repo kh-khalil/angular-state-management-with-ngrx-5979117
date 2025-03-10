f8847d2ac8ca01f7230d973cb679eb5dc0768d08
alcfeoh <alain@interstate21.com>
2025-03-10 15:02:06 -0700

    Video 2-09

---
+++ 02_09e/src/app/current-conditions/current-conditions.component.ts
@@ -0,0 +1,30 @@
+import {Component, inject} from '@angular/core';
+import {Store} from '@ngrx/store';
+import {zipCodesFeatureKey} from '../reducers/zip-codes.reducer';
+import {AsyncPipe, DecimalPipe, KeyValuePipe} from '@angular/common';
+import {WeatherService} from '../weather.service';
+import {CurrentConditionsState} from '../reducers/current-conditions.reducer';
+import {ZipCodeActions} from '../actions/zip-code.actions';
+
+@Component({
+  selector: 'app-current-conditions',
+  templateUrl: './current-conditions.component.html',
+  imports: [
+    DecimalPipe,
+    KeyValuePipe
+  ],
+  styleUrls: ['./current-conditions.component.css']
+})
+export class CurrentConditionsComponent {
+
+  private store = inject(Store);
+  currentConditions = this.store.selectSignal<CurrentConditionsState>(state => state.currentConditions);
+
+  weatherService = inject(WeatherService);
+
+  removeZip(zipcode: string) {
+    this.store.dispatch(ZipCodeActions.removeZipCode({zipcode}));
+  }
+
+
+}
---
+++ 02_09e/src/app/forecasts-list/forecasts-list.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+import { RouterLink} from '@angular/router';
+
+@Component({
+  selector: 'app-forecasts-list',
+  templateUrl: './forecasts-list.component.html',
+  imports: [
+    RouterLink
+  ],
+  styleUrls: ['./forecasts-list.component.css']
+})
+export class ForecastsListComponent {
+
+}
